538d6191596a4237495d1050f53de3845bccb699
janek <janek.thomas@web.de>
2016-07-11 22:44:57 +0200

    fix wrong test functions

--- tests/testthat/test_learners_all_surv.R
+++ tests/testthat/test_learners_all_surv.R
@@ -36,5 +36,5 @@ test_that("learners work: surv ", {
 
   # survival variable importance
   lrns = mylist("surv", properties = "featimp", create = TRUE)
-  lapply(lrns, testThatLearnerHandlesMissings, task = surv.task, hyperpars = hyperpars)
+  lapply(lrns, testThatLearnerCanCalculateImportance, task = surv.task, hyperpars = hyperpars)
 })
